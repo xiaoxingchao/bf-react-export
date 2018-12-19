b7dfa805827b70118d4f3cebc245b99b247d2f7b
xxc <549021324@qq.com>
2018-12-19 13:40:12 +0800

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
-import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
 export default {
   input: './src/main.js',
   output: {
@@ -7,9 +12,20 @@ export default {
     format: 'cjs'
   },
   plugins: [
-    resolve(),
+    resolve({
+      jsnext: true,  // 该属性是指定将Node包转换为ES2015模块
+      // main 和 browser 属性将使插件决定将那些文件应用到bundle中
+      main: true,  // Default: true 
+      browser: true // Default: false
+    }),
+    commonjs(),
+    json(),
     babel({
-      exclude: 'node_modules/**'  // 排除node_module下的所有文件
-    })
+      exclude: 'node_modules/**'  // 排除node_modules 下的文件
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    (process.env.NODE_ENV === 'production' && uglify())
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import { a } from './js/a';
 import addArray from './js/b';
+// import XLSX from 'xlsx'
+import debug from 'debug';
+
+// 添加json
+import pkg from '../package.json';
+console.log( `running version ${pkg.dependencies}` ); // 控制台输出 running version 1.0.0
+
+const log = debug('app:log');
+
+if (ENV !== 'production') {
+  // Enable the logger.
+  debug.enable('*');
+  log('Logging is enabled!');
+} else {
+  debug.disable();
+}
+
 
 const res1 = a('kongzhi');
 const res2 = addArray([1, 2, 3, 4]);
 
-console.log(res1);
-console.log(res2);
+
+// Print the results on the page.
+const printTarget = document.getElementsByClassName('debug__output')[0];
+
+printTarget.innerText = `sayHelloTo('Jason') => ${res1}\n\n`;
+printTarget.innerText += `addArray([1, 2, 3, 4]) => ${res2}`;
