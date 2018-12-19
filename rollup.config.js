164bd59d538edb0741f44971468e48be5c1faf4d
xxc <549021324@qq.com>
2018-12-19 15:38:16 +0800

    1.1

--- rollup.config.js
+++ rollup.config.js
@@ -27,6 +27,6 @@ export default {
     replace({
       ENV: JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    ((process.env.NODE_ENV.indexOf('production')!==-1) && uglify())
+    uglify()
   ]
 }
